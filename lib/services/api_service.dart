d0b44018da7e70d522ad76fd3e086805c8ea30fc
DAAC-A00 <wnsdudrkwh@gmail.com>
2023-12-19 16:43:30 +0900

    <feat> api async await 기본 예시 1

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,19 +4,23 @@ import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+  static String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  static String today = "today";
 
-  void getTodayTooms() async {
+  static Future<List<WebtoonModel>> getTodayTooms() async {
+    List<WebtoonModel> webtoonInstances = [];
+
     final url = Uri.parse("$baseUrl/$today");
     final response = await http.get(url);
+
     if (response.statusCode == 200) {
       final List<dynamic> webtoonList = jsonDecode(response.body);
+
       for (var webtoon in webtoonList) {
         final toon = WebtoonModel.fromJson(webtoon);
-        print(toon.title);
+        webtoonInstances.add(toon);
       }
-      return;
+      return webtoonInstances;
     } else {
       throw Error;
     }
