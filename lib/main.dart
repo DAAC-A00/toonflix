c2032da13be5113953cea53393f917b79dde16c1
DAAC-A00 <wnsdudrkwh@gmail.com>
2023-12-19 11:09:06 +0900

    <feat> http install, http get 초기 sample 구현

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix/screens/home_screen.dart';
+import 'package:toonflix/services/api_service.dart';
+
+void main() {
+  ApiService().getTodayTooms();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        child: const HomeScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        elevation: 3,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        centerTitle: true,
+        title: const Text(
+          "Today's 툰s",
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
