8daad1ecf99ba01b2c250b7e254f1e5d881b495e
DAAC-A00 <wnsdudrkwh@gmail.com>
2023-12-21 11:31:57 +0900

    <feat> toon 목록 좌우 : 그림자 넣기, padding 여백 넣기

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -50,6 +50,7 @@ class HomeScreen extends StatelessWidget {
         width: 20,
       ),
       itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
       itemBuilder: (context, index) {
         print(index);
         var toon = snapshot.data![index];
@@ -58,10 +59,20 @@ class HomeScreen extends StatelessWidget {
             Container(
               width: 150,
               clipBehavior: Clip.hardEdge,
-              decoration:
-                  BoxDecoration(borderRadius: BorderRadius.circular(20)),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 15,
+                      offset: const Offset(7, 20),
+                      color: Colors.red.withOpacity(0.5),
+                    )
+                  ]),
               child: Image.network(
                   "https://img1.daumcdn.net/thumb/C428x428/?scode=mtistory2&fname=https%3A%2F%2Ftistory1.daumcdn.net%2Ftistory%2F3072371%2Fattach%2F04db9f1b99d5485aba3ab29abd95b154"),
+            ),
+            const SizedBox(
+              height: 20,
             ),
             Text(
               toon.title,
